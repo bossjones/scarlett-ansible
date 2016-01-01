f5b0735ab6728b6e5719828b6a9dfa2c078240bb
Malcolm Jones <malcolm@adobe.com>
2016-01-01 16:19:32 -0500

    new: working serverspec for vagrant

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,30 @@
+# require 'serverspec'
+
+# set :backend, :exec
+
 require 'serverspec'
+require 'net/ssh'
+require 'tempfile'
 
-set :backend, :exec
+set :backend, :ssh
 
-# RSpec.configure do |config|
-#   config.expect_with :rspec do |c|
-#     c.syntax = :expect
-#   end
+host = ENV['TARGET_HOST']
+
+`VAGRANT_VAGRANTFILE=Vagrantfile-ansible-test vagrant up #{host}`
+
+config = Tempfile.new('', Dir.tmpdir)
+`VAGRANT_VAGRANTFILE=Vagrantfile-ansible-test vagrant ssh-config #{host} > #{config.path}`
+
+options = Net::SSH::Config.for(host, [config.path])
+
+options[:user] ||= Etc.getlogin
+
+set :host,        options[:host_name] || host
+set :ssh_options, options
+
+# begin
+#   Gem::Specification.find_by_name('yaml')
+# rescue Gem::LoadError
+#   require 'rubygems/dependency_installer'
+#   Gem::DependencyInstaller.new(Gem::DependencyInstaller::DEFAULT_OPTIONS).install('yaml')
 # end
-
-begin
-  Gem::Specification.find_by_name('yaml')
-rescue Gem::LoadError
-  require 'rubygems/dependency_installer'
-  Gem::DependencyInstaller.new(Gem::DependencyInstaller::DEFAULT_OPTIONS).install('yaml')
-end
