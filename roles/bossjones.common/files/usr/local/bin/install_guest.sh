97ceccd45d28fa21a1e2c89453de79fd4f11cc25
Malcolm Jones <malcolm@adobe.com>
2016-01-07 19:48:52 -0500

    chg: guest additions

--- roles/bossjones.common/files/usr/local/bin/install_guest.sh
+++ roles/bossjones.common/files/usr/local/bin/install_guest.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-wget http://download.virtualbox.org/virtualbox/5.0.10/VBoxGuestAdditions_5.0.10.iso && \
-sudo mkdir /media/VBoxGuestAdditions && \
-sudo mount -o loop,ro VBoxGuestAdditions_5.0.10.iso /media/VBoxGuestAdditions && \
-sudo sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run && \
-rm VBoxGuestAdditions_5.0.10.iso && \
-sudo umount /media/VBoxGuestAdditions && \
-sudo rmdir /media/VBoxGuestAdditions
+wget http://download.virtualbox.org/virtualbox/5.0.10/VBoxGuestAdditions_5.0.10.iso;
+mkdir /media/VBoxGuestAdditions;
+mount -o loop,ro VBoxGuestAdditions_5.0.10.iso /media/VBoxGuestAdditions;
+sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run;
+rm VBoxGuestAdditions_5.0.10.iso;
+umount /media/VBoxGuestAdditions;
+rmdir /media/VBoxGuestAdditions;
