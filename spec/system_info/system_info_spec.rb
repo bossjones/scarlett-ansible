9d05761280b5b3f19f6bec51e98f6b351a2b7999
Malcolm Jones <malcolm@adobe.com>
2016-02-07 17:44:17 -0500

    chg: working gstreamer tests on 14.x and 15.x

---
+++ spec/system_info/system_info_spec.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+require 'spec_helper'
+
+describe "BOSSSSSS JONESSSSS #{os[:release]} #{os[:family]} #{os[:arch]} #{os[:release].to_i}" do
+  it 'installs python' do
+    expect(package 'python').to be_installed
+  end
+end
