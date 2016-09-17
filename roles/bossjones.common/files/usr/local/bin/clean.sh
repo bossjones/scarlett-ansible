8e617a2979d3d201a470ac6c324384ef72d500ed
Malcolm Jones <malcolm@adobe.com>
2016-09-17 11:41:10 -0400

    rm: bossjones.common

--- roles/bossjones.common/files/usr/local/bin/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-apt-get autoclean -y && \
-apt-get autoremove -y && \
-rm -rf /tmp/* /var/tmp/* && \
-rm -rf /var/lib/apt/lists/*
--- roles/bossjones.common/files/usr/local/bin/install_guest.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-wget http://download.virtualbox.org/virtualbox/5.0.10/VBoxGuestAdditions_5.0.10.iso;
-mkdir /media/VBoxGuestAdditions;
-mount -o loop,ro VBoxGuestAdditions_5.0.10.iso /media/VBoxGuestAdditions;
-sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run;
-rm VBoxGuestAdditions_5.0.10.iso;
-umount /media/VBoxGuestAdditions;
-rmdir /media/VBoxGuestAdditions;
